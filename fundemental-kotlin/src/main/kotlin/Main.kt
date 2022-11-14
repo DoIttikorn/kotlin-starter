54cef22d92a3b9a76aa64a3bf79aa7e35947ce14
ittikorn chawkamud <ittikorn.chawkamud@scbtechx.io>
2022-11-14 15:33:15 +0700

    string

--- fundemental-kotlin/src/main/kotlin/Main.kt
+++ fundemental-kotlin/src/main/kotlin/Main.kt
@@ -3,9 +3,8 @@ package main.kotlin
 import main.kotlin.basic.number
 import main.kotlin.basic.boolean
 import main.kotlin.basic.characters
+import main.kotlin.basic.string
 
 fun main(args: Array<String>) {
-    number()
-    boolean()
-    characters()
+    string()
 }
--- fundemental-kotlin/src/main/kotlin/basic/string.kt
+++ fundemental-kotlin/src/main/kotlin/basic/string.kt
@@ -1,5 +1,53 @@
 package main.kotlin.basic
 
 fun string() {
+    val str = "abda 1123"
+    println(str.uppercase()) // Crate new String object not mutation old object String
+    println(str)
+    for (c in str) {
+        println(c)
+    }
 
+//    concatenate string use + (plus)
+    val s: String = "asb" + 1
+    println(s + "def")
+
+    StringLiterals()
+    StringTemplate()
+}
+
+fun StringLiterals() {
+//    2 type
+//    1 Escaped string
+//    2 Raw string
+
+//    Escaped string
+    /*
+    Escaping is donw in the conventional way, with a backslash (\)
+     */
+    val s = "Hello, world ? \n"
+
+
+
+// Raw string
+    val text = """
+        |Tell me and I forget.
+        |Teach me and I remember.
+        |Involve me and I learn.
+        |(Benjamin Franklin)
+    """.trimIndent()
+    println(text)
+
+
+}
+
+fun StringTemplate() {
+    val s = "abc"
+    println("$s.length is ${s.length}") // Prints "abc.length is 3"
+
+    val price = """
+    ${'$'}_9.99
+    """
+
+    println(price)
 }
